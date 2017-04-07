c6bdae84e52e0c71fa4fec33e0ea7d1c076b7707
swathigoru <swathi.goru@gmail.com>
2017-04-06 18:46:03 -0700

    day 4

---
+++ StringOperations.py
@@ -0,0 +1,3 @@
+str1 = raw_input("Enter 1st string:")
+str2 = raw_input("Enter 2nd string:")
+print (str1[1:] + str2[1:])
---
+++ calculateGrossPay.py
@@ -0,0 +1,4 @@
+hours = raw_input("Enter # of hours:")
+rate = raw_input("Enter rate per hour:")
+gross_pay = float(hours) * float(rate)
+print "Gross pay is %f" % (gross_pay)
