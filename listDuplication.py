6088ada2e564d4fa43fd1020347cde799046fdd4
swathigoru <swathi.goru@gmail.com>
2017-04-13 23:10:07 -0700

    variable names changed

--- listDuplication.py
+++ listDuplication.py
@@ -6,12 +6,12 @@ Write a program to print a new list which contains only common elements from bot
 
 a = [1,2,2,3,4,13,18,26,38,66]
 b= [1,1,2,5,13,16,26,33,43,49,59,66]
-list = []
+list1 = []
 i = 0
 x = 0
 
 for i in range(len(b)):
     for x in range(len(a)):
         if a[x] == b[i]:
-            list.append(b[i])
-print list
+            list1.append(b[i])
+print list1
