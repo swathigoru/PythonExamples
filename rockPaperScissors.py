b9ed304f8134a216179e9819b4aab5c12788c684
swathigoru <swathi.goru@gmail.com>
2017-04-13 23:07:02 -0700

    day 8

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -3,11 +3,22 @@
 print out a message of congratulations to the winner, 
 and ask if the players want to start a new game)"""
 
-player1 = raw_input("Player 1: Enter your choice:")
-player2 = raw_input("Player 2: Enter your choice: ")
+choice = 'y'
 
-if (player1 == "rock" and player2 == "paper"):
+while (choice == 'y'):
+    player1 = raw_input("Player 1: Enter your choice:")
+    player2 = raw_input("Player 2: Enter your choice: ")
+    if (player1 == "rock" and player2 == "paper"):
         print "Player 2 wins!"
-elif (player1 == "rock" and player2 == "scrissors"):
+    elif (player1 == "paper" and player2 == "scissors"):
+        print "Player 2 wins!"
+    elif (player1 == "scissors" and player2 == "rock"):
+        print "Player 2 wins!"
+    else:
         print "Player 1 wins!"
+    choice = raw_input("Do you want to play another game? (y/n): ")
+    continue
+print "End of the Game"
+ 
+
         
